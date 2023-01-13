5be532975e7b0fa2449006621ca19ac653bd1608
Rafael Milléo <rafael@milleo.net>
2023-01-13 18:08:42 -0300

    Creating topics table

---
+++ src/components/GenresTable.js
@@ -0,0 +1,43 @@
+import { Table } from "react-bootstrap";
+import topicsData from "../data/topics.json";
+import { convertNumberToSymbols } from "../utils";
+
+const GenresTable = () => {
+    return <Table striped bordered hover>
+        <thead>
+            <tr>
+                <th rowSpan={2}>Topic</th>
+                <th colSpan={6}>Genres</th>
+                <th colSpan={3}>Audiences</th>
+            </tr>
+            <tr>
+                <th>Action</th>
+                <th>Adventure</th>
+                <th>RPG</th>
+                <th>Simulation</th>
+                <th>Strategy</th>
+                <th>Casual</th>
+                <th>Y</th>
+                <th>E</th>
+                <th>M</th>
+            </tr>
+        </thead>
+        <tbody>
+        { topicsData.map((topic) => {
+            return <tr>
+                <td>{ topic.name }</td>
+                { Object.keys(topic.genres).map((genre) => { return <td className={`value${topic.genres[genre]}`}>
+                        { convertNumberToSymbols(topic.genres[genre]) }
+                    </td>
+                })}
+                { Object.keys(topic.audience).map((aud) => { return <td className={`value${topic.audience[aud]}`}>
+                    { convertNumberToSymbols(topic.audience[aud]) }
+                    </td>
+                })}
+            </tr>
+        }) }
+        </tbody>
+    </Table>
+}
+
+export default GenresTable;
