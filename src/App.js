4a8ca17a45a3d96a368d3fc469b18e48d5441b60
Rafael Milléo <rafael@milleo.net>
2023-01-13 19:22:14 -0300

    Filtering by topic, genre and audience

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import { useState } from 'react';
 
 function App() {
   const [topicsDataFiltered, setTopicsDataFiltered] = useState(topicsData);
+  const [filteredGenres, setFilteredGenres] = useState(["action", "adventure", "rpg", "simulation", "strategy", "casual"]);
+  const [filteredAudiences, setFilteredAudiences] = useState(["young", "everyone", "adult"]);
 
   const handleChangeTopic = (values) => {
     const valuesArr = values.map((v) => v.value);
-    if(valuesArr.length == 0){
+    if(valuesArr.length === 0){
       setTopicsDataFiltered(topicsData);
       return;
     }
@@ -19,10 +21,55 @@ function App() {
     setTopicsDataFiltered(result);
   }
 
+  const handleChangeGenre = (values) => {
+    const valuesArr = values.map((v) => v.value);
+    setFilteredGenres(valuesArr);
+    if(valuesArr.length === 0){
+      setTopicsDataFiltered(topicsData);
+      return;
+    }
+    
+    const result = topicsData.map((topic, index) => {
+      return {
+        name: topic.name,
+        genres: Object.keys(topicsData[index].genres)
+          .filter((genre) => valuesArr.indexOf(genre) > -1)
+          .reduce((obj, key) => {
+            return Object.assign(obj, { [key]: topic.genres[key] })
+          }, {}),
+        audience: topicsData[index].audience
+      };
+    });
+
+    setTopicsDataFiltered(result);
+  }
+  const handleChangeAudience = (values) => {
+    const valuesArr = values.map((v) => v.value);
+    setFilteredAudiences(valuesArr);
+    if(valuesArr.length === 0){
+      setTopicsDataFiltered(topicsData);
+      return;
+    }
+    
+    const result = topicsData.map((topic, index) => {
+      return {
+        name: topic.name,
+        audience: Object.keys(topicsData[index].audience)
+          .filter((genre) => valuesArr.indexOf(genre) > -1)
+          .reduce((obj, key) => {
+            return Object.assign(obj, { [key]: topic.audience[key] })
+          }, {}),
+        genres: topicsData[index].genres
+      };
+    });
+
+    setTopicsDataFiltered(result);
+  }
+
   return (
     <div className="App">
-      <FilterForm onChangeTopic={ handleChangeTopic } />
-      <GenresTable data={topicsDataFiltered} />
+      <FilterForm onChangeTopic={ handleChangeTopic } onChangeGenre={ handleChangeGenre } onChangeAudience={ handleChangeAudience } />
+      <GenresTable data={topicsDataFiltered} filteredGenres={ filteredGenres } filteredAudiences={ filteredAudiences } />
     </div>
   );
 }
--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -1,19 +1,45 @@
-import { Card, Container, FloatingLabel, Form } from "react-bootstrap";
+import { Card, Col, Container, Form, Row } from "react-bootstrap";
 import Select from 'react-select';
 import topicsData from "../data/topics.json";
 
 const FilterForm = (props) => {
-    const { onChangeTopic } = props;
+    const { onChangeGenre, onChangeTopic, onChangeAudience } = props;
     const topicsOptions = topicsData.map((topic) => {
         return { value: topic.name, label: topic.name };
     });
+    const genresOptions = Object.keys(topicsData[0].genres).map((genre) => {
+        return { value: genre, label: genre };
+    });
+    const audiencesOptions = Object.keys(topicsData[0].audience).map((aud) => {
+        return { value: aud, label: aud };
+    });
     
     return <Container>
-        <Card>
+        <Card className="my-3">
             <Card.Header>Filter</Card.Header>
-            <Form>
-                <Form.Label>Topic</Form.Label>
-                <Select onChange={onChangeTopic} options={topicsOptions} isMulti></Select>
+            <Form className="p-4">
+                <Row>
+                    <Col md="12">
+                        <Form.Group className="mb-3">
+                            <Form.Label>Topic</Form.Label>
+                            <Select onChange={onChangeTopic} options={topicsOptions} isMulti></Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md="8">
+                        <Form.Group className="mb-3">
+                            <Form.Label>Genres</Form.Label>
+                            <Select onChange={onChangeGenre} options={genresOptions} isMulti></Select>
+                        </Form.Group>
+                    </Col>
+                    <Col md="4">
+                        <Form.Group className="mb-3">
+                            <Form.Label>Audience</Form.Label>
+                            <Select onChange={onChangeAudience} options={audiencesOptions} isMulti></Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
             </Form>
         </Card>
     </Container>
