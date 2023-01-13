308a46f5c9b99e89fa13dd8eccdc24df9626f067
Rafael Milléo <rafael@milleo.net>
2023-01-13 18:08:58 -0300

    Creating form to filter values

---
+++ src/components/FilterForm.js
@@ -0,0 +1,12 @@
+import { Card, Form } from "react-bootstrap";
+
+const FilterForm = () => {
+    return <Card>
+        <Card.Header>Filter</Card.Header>
+        <Form>
+            
+        </Form>
+    </Card>
+}
+
+export default FilterForm;
