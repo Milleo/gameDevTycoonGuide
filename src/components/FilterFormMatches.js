4e3a4427ceb8a510e27c11b1fb8b701e96f6fd00
Rafael Milléo <rafael@milleo.net>
2023-01-16 16:02:11 -0300

    Fixing use effect incorrect usage

--- src/components/FilterFormMatches.js
+++ src/components/FilterFormMatches.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Card, Col, Form, Row } from "react-bootstrap";
 import { useIntl } from "react-intl";
 import Select from 'react-select';
@@ -17,7 +17,25 @@ const FilterFormMatches = (props) => {
     const topicsOptions = topicsData.map((topic) => { return { value: topic.name, label: topic.name } });
     const platformOptions = platformsData.map((platform) => { return { value: platform.name, label: platform.name } });
 
-    useEffect(() => onChange(platforms, topics, audienceAvailable, casualAvailable), [topics, platforms, audienceAvailable, casualAvailable, onChange])
+    const handleChange = (value, field) => {
+        switch(field){
+            case "platforms":
+                setPlatforms(value);
+                break;
+            case "topics":
+                setTopics(value);
+                break;
+            case "audienceAvailable":
+                setAudienceAvailable(value);
+                break;
+            case "casualAvailable":
+                setCasualAvailable(value);
+                break;
+            default:
+                break;
+        }
+        onChange(platforms, topics, audienceAvailable, casualAvailable);
+    }
     
     return <Row>
         <Col md="6">
@@ -26,15 +44,15 @@ const FilterFormMatches = (props) => {
             <Form className="p-4">
                 <Form.Group className="mb-3">
                     <Form.Label>{ t({id: "filter.matches.platforms" })}</Form.Label>
-                    <Select className="multiSelect" onChange={setPlatforms} options={platformOptions} isMulti></Select>
+                    <Select className="multiSelect" onChange={(values) => handleChange(values, "platforms")} options={platformOptions} isMulti></Select>
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>{ t({id: "filter.matches.topics" })}</Form.Label>
-                    <Select className="multiSelect" onChange={setTopics} options={topicsOptions} isMulti></Select>
+                    <Select className="multiSelect" onChange={(values) => handleChange(values, "topics")} options={topicsOptions} isMulti></Select>
                 </Form.Group>
                 <Form.Group className="mb-3">
-                    <Form.Check onClick={(e) => setAudienceAvailable(e.target.checked)} type="switch" label={ t({id: "filter.matches.audience" })} id="audience-available" />
-                    <Form.Check onClick={(e) => setCasualAvailable(e.target.checked)} type="switch" label={ t({id: "filter.matches.casual" })} id="casual-games-available" />
+                    <Form.Check onClick={(e) => handleChange(e.target.checked, "audienceAvailable")} type="switch" label={ t({id: "filter.matches.audience" })} id="audience-available" />
+                    <Form.Check onClick={(e) => handleChange(e.target.checked, "casualAvailable")} type="switch" label={ t({id: "filter.matches.casual" })} id="casual-games-available" />
                 </Form.Group>
             </Form>
         </Card>
